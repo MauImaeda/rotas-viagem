efa1da0af73d5df5fff54038497b3046abbaa9cf
Mauricio Imaeda <>
2021-01-18 10:57:01 -0200

    deletado arquivos desnecessarios e corrigido rota de buscar rota

--- src/routes/rotaRoutes.js
+++ src/routes/rotaRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/rotaController')
-router.get('/', controller.get);
+router.get('/buscar-rota', controller.get);
 router.post('/adicionar-rota', controller.post);
 module.exports = router;
